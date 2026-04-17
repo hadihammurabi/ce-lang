6ad2be0f4bfc3998390809a63da545675512f8fa
hadihammurabi <hadihammurabi@gmail.com>
2026-04-17 22:45:00 +0700

    feat(lsp.completion): send completion list

--- bin/celsp.ml
+++ bin/celsp.ml
@@ -29,17 +29,19 @@ let publish_diags notify_back uri src =
         let start_pos = Position.create ~line ~character:col in
         let end_pos = Position.create ~line ~character:(col + 1) in
         let range = Range.create ~start:start_pos ~end_:end_pos in
-
         let diagnostic = Diagnostic.create ~range ~message:(`String msg) () in
-
         [ diagnostic ]
   in
   notify_back#send_diagnostic diags
 
 class ce_lsp_server =
   object (self)
-    inherit Jsonrpc2.server
+    inherit Jsonrpc2.server as super
     method spawn_query_handler f = Lwt.async f
+
+    method! config_completion =
+      Some
+        (Linol_lsp.Lsp.Types.CompletionOptions.create ~resolveProvider:false ())
 
     method on_notif_doc_did_open ~notify_back d ~content =
       log "on_notif_doc_did_open: checking syntax...";
@@ -51,6 +53,75 @@ class ce_lsp_server =
         =
       log "on_notif_doc_did_change: file changed, running parser...";
       publish_diags notify_back d.uri new_content
+
+    method! on_req_completion ~notify_back:_ ~id:_ ~uri:_ ~pos:_ ~ctx:_
+        ~workDoneToken:_ ~partialResultToken:_ _doc_state =
+      log "Autocomplete requested!";
+
+      let create_keyword label =
+        CompletionItem.create ~label ~kind:CompletionItemKind.Keyword ()
+      in
+
+      let create_type label =
+        CompletionItem.create ~label ~kind:CompletionItemKind.TypeParameter ()
+      in
+
+      let keywords =
+        List.map create_keyword
+          [
+            "let";
+            "mut";
+            "if";
+            "else";
+            "for";
+            "break";
+            "return";
+            "import";
+            "type";
+            "impl";
+            "raise";
+            "catch";
+            "struct";
+            "trait";
+            "true";
+            "false";
+            "nil";
+          ]
+      in
+
+      let types =
+        List.map create_type
+          [
+            "int";
+            "string";
+            "bool";
+            "float";
+            "char";
+            "void";
+            "i8";
+            "i16";
+            "i32";
+            "i64";
+            "i128";
+            "uint";
+            "u8";
+            "u16";
+            "u32";
+            "u64";
+            "u128";
+            "f32";
+            "f64";
+          ]
+      in
+
+      let fn_snippet =
+        CompletionItem.create ~label:"fn" ~kind:CompletionItemKind.Snippet
+          ~insertText:"fn ${1:name}(${2}) ${3:void} {\n  $0\n}"
+          ~insertTextFormat:InsertTextFormat.Snippet
+          ~detail:"Define a new function" ()
+      in
+      let items = fn_snippet :: (keywords @ types) in
+      Lwt.return_some (`List items)
   end
 
 let execute () =
